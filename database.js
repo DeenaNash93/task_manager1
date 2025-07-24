92d44e7d57ca9c30a57f701f88d50d88d8599907
deena_nash <136482216+DeenaNash93@users.noreply.github.com>
2025-07-24 16:01:03 +0300

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '', // אם יש לך סיסמה, שימי אותה כאן
+    password: '',
     database: 'task_manager_db',
     waitForConnections: true,
     connectionLimit: 10,
